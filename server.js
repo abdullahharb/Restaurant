abd0e5f0c053afa4cc8983d1040a034be66be952
Abdullah <harb507090@gmail.com>
2026-08-24 20:13:01 +0300

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import { dbConnection } from "./databases/dbConnection.js"
 import { Server } from 'socket.io'
 import { orderSocket } from "./src/modules/order/order.socket.js"
 import { init } from "./src/modules/index.route.js"
+import cors from 'cors'
+
 
 dotenv.config({ quiet: true })
 const app = express()
@@ -11,11 +13,14 @@ const app = express()
 app.set('query parser', 'extended')
 app.use(express.json())
 app.use(express.static('uploads'))
+app.use(cors())
+
 
 init(app)
 dbConnection()
 
-const server = app.listen(5000, () => console.log('server is running...'))
+const server = app.listen(process.env.PORT || 5000, () => console.log('server is running...'))
+
 const io = new Server(server, { cors: { origin: '*' } })
 app.set('io', io)
 orderSocket(io)
